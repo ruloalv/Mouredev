b5d2fe9eedc47a76405af946cda2e495d696e72c
ruloalv <ruloalv@gmail.com>
2023-01-05 16:17:08 -0300

    filter user by id

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -7,14 +7,15 @@ app = FastAPI()
 
 # Entidad User
 class  User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users_list = [User(name="Raul", surname="Alvarado", url="https://ruloalv.dev", age=37),
-        User(name="Bruno", surname="Parodi", url="https://bruno.pt", age=35),
-        User(name="Facu", surname="Orazi", url="https://facu.put", age=33)]
+users_list = [User(id=1, name="Raul", surname="Alvarado", url="https://ruloalv.dev", age=37),
+        User(id=2, name="Bruno", surname="Parodi", url="https://bruno.pt", age=35),
+        User(id=3, name="Facu", surname="Orazi", url="https://facu.put", age=33)]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -25,3 +26,10 @@ async def usersjson():
 @app.get("/users")
 async def users():
     return users_list
+
+@app.get("/user/{id}")
+async def user(id: int):
+    user = filter(lambda user: user.id == id, users_list)
+    #user = [user for user in users_list if user.id == id]
+    print (user)
+    return user
