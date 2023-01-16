cdc34fc68fc06bc4a0377c7dbc4545ce54d3f14f
ruloalv <ruloalv@gmail.com>
2023-01-16 15:42:04 -0300

    add response model

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# Inicia el server: python -m uvicorn main:app --reload
+# Detener el server: CTRL + C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
