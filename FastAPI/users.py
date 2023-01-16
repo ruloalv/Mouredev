d51965ab8182fb29b18be7296b93d0cd772595b6
ruloalv <ruloalv@gmail.com>
2023-01-16 14:49:55 -0300

    Add Query - post/put/delete

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -27,8 +27,46 @@ async def usersjson():
 async def users():
     return users_list
 
+#Path
 @app.get("/user/{id}")
 async def user(id: int):
+    return search_user(id)
+
+#Query
+@app.get("/user/")
+async def user(id: int):
+    return search_user(id)
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    users_list.append(user)
+    return user
+
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error": "No se ha encontrado el usuario"}
+    else:
+        return user
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+
+def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     #user = [user for user in users_list if user.id == id]
     try:
