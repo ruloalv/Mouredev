8edd678ecfe77ac5ff9c2b8197ea7f8ad89ebde0
ruloalv <ruloalv@gmail.com>
2023-01-17 15:53:25 -0300

    Manage Statics elements

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Inicia el server: python -m uvicorn main:app --reload
 # Detener el server: CTRL + C
