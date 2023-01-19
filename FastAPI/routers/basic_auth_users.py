06ac87de457e8c3fa1f0f50445d7a9f2ee671570
ruloalv <ruloalv@gmail.com>
2023-01-19 10:40:24 -0300

    finish basic_auth_user

--- FastAPI/routers/basic_auth_users.py
+++ FastAPI/routers/basic_auth_users.py
@@ -33,9 +33,13 @@ users_db = {
     }
 }
 
-def search_user(username: str):
+def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
+
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
 
 async def current_user(token: str = Depends(oauth2)):
     user = search_user(token)
@@ -44,6 +48,10 @@ async def current_user(token: str = Depends(oauth2)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciales de autenticación inválidas",
             headers={"WWW-Authenticate": "Bearer"})
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario inactivo")
     return user
 
 @app.post("/login")
@@ -52,7 +60,7 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     if not user_db:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
     
-    user = search_user(form.username)
+    user = search_user_db(form.username)
     if not user.password == form.password:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
     
